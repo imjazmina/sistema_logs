8f03fb5cc9ecb68e3cb4aa3ef20125fb9140ae6b
maria <alonsomoralesjazmin@gmail.com>
2025-09-08 20:10:22 -0300

    cliente

--- client.py
+++ client.py
@@ -1,20 +1,39 @@
 import random
-from datetime import date
+import requests
+import datetime
+import time
 
-mensaje  = ["Inicio de sesion exitosa", "Compra realizada con exito", "Error al crear usuario"]
+url_server = "http://127.0.0.1:5000/logs" #definir endpoint para recuperar datos, mi servidor
+
 servicios = ["Inicio sesion", "Alta usuario", "Compra"]
 severidad = ["info", "warning", "error"]
-fecha = date.today()
-
-token = "fakeToken2.0"
 
 def crear_logs():
     log = {
-    "timestamp": fecha.strftime("%d-%m-%Y"),
-    "servicio": random.choice(servicios),
+    "timestamp": datetime.utcnow().isoformat(),#fecha y hora que se envio
+    "service": random.choice(servicios),
     "severity": random.choice(severidad),
-    "messaje": random.choice(mensaje)
+    "message": "This is the log we created."
     }
     return log
 
-crear_logs()
+def enviar_logs(cantidad, delay= 1):
+        
+    for i in range(cantidad):
+        log = crear_logs()
+        try:
+            r = requests.post(#envia el json de loggssss
+                url_server, 
+                json=log,#ara indicar a la API que estamos enviando explícitamente un objeto JSON a la URL especificada. 
+                headers={"Autorizacion": "fakeToken2.0"} #agrega un header al dicc
+            )
+            if r.status_code == 200:#si la respuesta es exitosa
+                print(f"Log enviado {r.status_code} : {r.json()}")#para almacenar los datos de la respuesta en un objeto diccionario
+            else:
+                print(f"Error al enviar el log {r.status_code} : {r.json()}")
+        except Exception as e:
+            print(f"Error de conexion {e}")
+        time.sleep(delay)
+
+if __name__ == "__main__":
+    enviar_logs(cantidad=5)
